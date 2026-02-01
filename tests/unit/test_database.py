afefc1129903b00fdacfd4a75242a0f7f28e2006
mmo80 <33737944+mohavro@users.noreply.github.com>
2026-02-01 05:21:25 +0000

    Add autocommit assertion in database test

--- tests/unit/test_database.py
+++ tests/unit/test_database.py
@@ -146,6 +146,7 @@ class TestSessionFactory:
         finally:
             session.close()
 
+        assert session.autocommit is False
 
 # ---------------------------------------------------------------------------
 # Database initialization tests
