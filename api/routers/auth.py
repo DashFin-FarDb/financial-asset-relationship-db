cc98da9193558e5a787c93d8ce1c4935f7e67105
mmo80 <33737944+mohavro@users.noreply.github.com>
2026-04-26 20:00:58 +0100

    Fix user authentication check condition

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -36,15 +36,16 @@ async def login_for_access_token(
         Token: A `Token` object containing the JWT in `access_token` and `token_type` set to "bearer".
     """
     user = authenticate_user(form_data.username, form_data.password)
-    if not user:
+    if user is False:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username},  # type: ignore[union-attr]
+        data={"sub": user.username},
         expires_delta=access_token_expires,
     )
     return Token(access_token=access_token, token_type="bearer")
