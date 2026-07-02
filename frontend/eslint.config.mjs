08b7e2dae6f5b41bfe2ebaa4e91ad56b664b9941
mmo80 <33737944+mohavro@users.noreply.github.com>
2026-07-02 04:10:32 +0100

    Update frontend/eslint.config.mjs
    Signed-off-by: mmo80 <33737944+mohavro@users.noreply.github.com>

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -52,7 +52,7 @@ export default tseslint.config(
       ...nextPlugin.configs['core-web-vitals'].rules,
       'react/react-in-jsx-scope': 'off',
       'react-hooks/exhaustive-deps': 'warn',
--      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/set-state-in-effect': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'error',
